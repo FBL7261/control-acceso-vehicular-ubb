5ca5ccb460d4543a1928333647316ab94b3c50d6
Cristopher Andrés Sáez Araneda <cristopher.saez2001@alumnos.ubiobio.cl>
2024-05-20 15:32:13 -0400

    Implementada restriccion de matricula, solo Mayusculas, numero y un total de 6 caracteres son permitidos

--- backend/src/schema/vehicle.schema.js
+++ backend/src/schema/vehicle.schema.js
@@ -2,7 +2,7 @@ import Joi from "joi"; // Framework para validación
 
 // Esquema para validar vehículos
 const vehicleSchema = Joi.object({
-  matricula: Joi.string().required().alphanum().length(6), // Verificación para matrículas
+  matricula: Joi.string().required().regex(/^[A-Z0-9]+$/).length(6).message('La matrícula solo debe contener letras mayúsculas y números, y tener una longitud de 6 caracteres'), // Verificación para matrículas
   modelo: Joi.string().required(), // Modelo requerido
   marca: Joi.string().required(), // Marca requerida
   color: Joi.string().required(), // Color requerido
