16b508492ac9f2c8aefe84b764be4c16aa9cff1e
Cristopher Andrés Sáez Araneda <cristopher.saez2001@alumnos.ubiobio.cl>
2024-07-19 14:23:14 -0400

    template

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./styles/styles.css";
+import ReactDOM from 'react-dom/client';
+import App from './routes/App.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/Root.jsx';
+import ErrorPage from './routes/ErrorPage.jsx';
+import Login from './routes/Login.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+    ],
+  },
+  {
+    path: '/auth',
+    element: <Login />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 );
